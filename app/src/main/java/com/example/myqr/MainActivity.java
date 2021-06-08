1e7e32e5a1ab83946a74fe555b8b94865999c4ff
coke0788 <coke0788@naver.com>
2021-06-08 11:56:27 +0900

    MainActivity 주석 추가

--- app/src/main/java/com/example/myqr/MainActivity.java
+++ app/src/main/java/com/example/myqr/MainActivity.java
@@ -15,15 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+        new IntentIntegrator(this).initiateScan(); //QR Scan라이브러리
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
+        if(result != null) { //결과가 null이 아닌 경우만
+            if(result.getContents() == null) { //콘텐츠가 널이면 취소
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
                 // todo
-            } else {
+            } else { //콘텐츠가 널이 아닌 경우 읽어오기. (토스트 팝업을 띄운다.)
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 // todo
             }
